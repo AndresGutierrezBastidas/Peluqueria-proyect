ef484bfe57d6950f3e31377313300af5e6d8098f
Miguel Perez <epsilonexe2@gmail.com>
2025-04-08 23:21:23 -0400

    Crear carpetas y prototipo de navbar

---
+++ peluqueria/src/app/components/landing-page/landing-services/landing-services.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ServiceCardComponent } from './service-card/service-card.component';
+
+@Component({
+  selector: 'landing-services',
+  imports: [ServiceCardComponent],
+  templateUrl: './landing-services.component.html',
+  styleUrl: './landing-services.component.css'
+})
+export class LandingServicesComponent {
+
+}
