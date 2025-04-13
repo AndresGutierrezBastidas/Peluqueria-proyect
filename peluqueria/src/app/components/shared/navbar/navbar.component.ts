19cbfb49abb54862d41ede4d945138cb6b9e5ce9
Miguel Pérez <epsilonexe2@gmail.com>
2025-04-13 16:12:55 -0400

    fix: rutas en navbar

--- peluqueria/src/app/components/shared/navbar/navbar.component.ts
+++ peluqueria/src/app/components/shared/navbar/navbar.component.ts
@@ -1,34 +1,20 @@
 import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ModalReservaHoraComponent } from '@componentes/modal-reserva-hora/modal-reserva-hora.component';
 
 @Component({
   selector: 'navbar',
   standalone: true,
-  imports: [ModalReservaHoraComponent],
+  imports: [ModalReservaHoraComponent,RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-    links = signal([{
-      link: '#inicio',
-      clases: 'text-gray-800 font-medium hover:text-amber-600 transition' ,
-      label: 'Inicio'
-    },
-    {
-      link: '#servicios',
-      clases: 'text-gray-800 font-medium hover:text-amber-600 transition' ,
-      label: 'Servicios'
-    },
-    {
-      link: '#contacto',
-      clases: 'text-gray-800 font-medium hover:text-amber-600 transition' ,
-      label: 'Contacto'
-    },
-    {
-      link: '#conoce',
-      clases: 'text-gray-800 font-medium hover:text-amber-600 transition' ,
-      label: 'Conocenos'
-    }]);
+
+
+
 }
+
+
 
 
