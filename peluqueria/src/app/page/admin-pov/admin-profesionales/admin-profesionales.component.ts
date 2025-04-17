ca3df4c6073a5b780af0357355d9ca13e12a0266
Miguel Pérez <epsilonexe2@gmail.com>
2025-04-17 05:30:21 -0400

    feat: Estructura de capertas y componentes de admin pov

---
+++ peluqueria/src/app/page/admin-pov/admin-profesionales/admin-profesionales.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-profesionales',
+  imports: [],
+  templateUrl: './admin-profesionales.component.html',
+  styleUrl: './admin-profesionales.component.css'
+})
+export class AdminProfesionalesComponent {
+
+}
---
+++ peluqueria/src/app/page/admin-pov/admin-servicios/admin-servicios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-servicios',
+  imports: [],
+  templateUrl: './admin-servicios.component.html',
+  styleUrl: './admin-servicios.component.css'
+})
+export class AdminServiciosComponent {
+
+}
