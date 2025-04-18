be66591c9981fd4e28c531b5cdf28c01a09d496c
RenatoXD4 <renatochicahual@gmail.com>
2025-04-18 18:19:19 -0400

    feature: "Usar swiper para el deslizado de profesionales en grupo. Falta el responsive"

--- peluqueria/src/app/components/modal-reserva-hora/primer-paso/profesionales/profesionales.component.ts
+++ peluqueria/src/app/components/modal-reserva-hora/primer-paso/profesionales/profesionales.component.ts
@@ -1,16 +1,28 @@
-import { Component, output } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, DestroyRef, inject, OnDestroy, OnInit, output, signal } from '@angular/core';
+import { register, SwiperContainer } from 'swiper/element/bundle';
+import {  SwiperOptions } from 'swiper/types';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import { Subject, takeUntil } from 'rxjs';
+// register Swiper custom elements
+register();
+
 
 @Component({
   selector: 'profesionales',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './profesionales.component.html',
-  styleUrl: './profesionales.component.css'
+  styleUrl: './profesionales.component.css',
 })
 export class ProfesionalesComponent {
   selectedProfessional: any = null;
   groupedProfessionals: any[][] = [];
   currentGroupIndex: number = 0;
   profesional = output<any>();
+  destroyed = new Subject<void>();
+  
+  swiperElement = signal<SwiperContainer | null>(null)
+
 
   professionals: any[] = [{
     id: 1,
@@ -49,19 +61,55 @@ export class ProfesionalesComponent {
   },
   ];
 
-  constructor() {
-    this.groupProfessionals();
+  constructor(private responsive: BreakpointObserver) {
+
+    this.Breakpoint()
+    
+    
+    const swiperContainer = document.querySelector('.swiper-container')
+    const swiperOptions: SwiperOptions = {
+      slidesPerView: 1,
+      spaceBetween: 16,
+      freeMode: true,
+    }
+
+    if(swiperContainer){
+      Object.assign(swiperContainer!, swiperOptions)
+      this.swiperElement.set(swiperContainer as SwiperContainer)
+      this.swiperElement()?.initialize
+    }
+
+    
   }
+  
+
+  Breakpoint(){
+    this.responsive.observe("grid-cols-5").pipe(takeUntil(this.destroyed)).subscribe(state => {
+      if(!state.matches){
+        console.log("Arreglo de 5")
+        this.groupProfessionals(5);
+      }else{
+        console.log("Arreglo de 3")
+        this.groupProfessionals(3);
+      }
+    })
+  }
+
+
+
+
 
   selectProfessional(professional: any) {
     this.selectedProfessional = professional;
     this.profesional.emit(this.selectedProfessional);
   }
 
-  groupProfessionals(){
-    const groupSize = 5;
-    for(let i = 0; i < this.professionals.length; i += groupSize) {
-      this.groupedProfessionals.push(this.professionals.slice(i, i + groupSize));
+
+  groupProfessionals(qty: number) {
+    const groupSize = qty;
+    for (let i = 0; i < this.professionals.length; i += groupSize) {
+      this.groupedProfessionals.push(this.professionals.slice(i, i + groupSize)
+      ); //Pushea el grupo de profesionales en este arreglo, que deberían ser 5 por cada slide.
     }
   }
 
