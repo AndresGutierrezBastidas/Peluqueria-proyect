758698840bc55b323ba89fd1bb54f778f4851903
sseba-silva <seba.silvan20@gmail.com>
2025-04-19 00:10:24 -0400

    mapa ubicacion y precio services

---
+++ peluqueria/src/app/components/landing-page/landing-map/landing-map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'landing-map',
+  imports: [],
+  templateUrl: './landing-map.component.html',
+  styleUrl: './landing-map.component.css'
+})
+export class LandingMapComponent  {
+
+}
--- peluqueria/src/app/page/landing-page/landing-page.component.ts
+++ peluqueria/src/app/page/landing-page/landing-page.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { LandingAboutUsComponent } from '../../components/landing-page/landing-about-us/landing-about-us.component';
 import { LandingServicesComponent } from '../../components/landing-page/landing-services/landing-services.component';
 import { LandingWelcomeComponent } from '../../components/landing-page/landing-welcome/landing-welcome.component';
+import { LandingMapComponent } from '../../components/landing-page/landing-map/landing-map.component';
 
 
 @Component({
   selector: 'landing-page',
   standalone: true,
-  imports: [LandingAboutUsComponent,LandingServicesComponent,LandingWelcomeComponent],
+  imports: [LandingAboutUsComponent,LandingServicesComponent,LandingWelcomeComponent,LandingMapComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
