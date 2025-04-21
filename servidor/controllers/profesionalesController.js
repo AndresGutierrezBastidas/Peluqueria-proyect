5d0a727f3cfe0a62c47bbd3862fae1eaba2d42bc
RenatoXD4 <renatochicahual@gmail.com>
2025-04-20 21:15:51 -0400

    fix:"Obtener json al llamar la API en postman"

--- servidor/controllers/profesionalesController.js
+++ servidor/controllers/profesionalesController.js
@@ -2,8 +2,8 @@ import { getProfesionales } from "../services/profesionalesService.js"
 
 export async function profesionales(req, res) {
     try{
-       const getProfs =  getProfesionales
-       return res.json(getProfs)
+       const getProfs =  getProfesionales()
+       res.status(200).json(getProfs)
     }catch(e){
         console.log('Error al obtener los profesionales', e.message)
     }
