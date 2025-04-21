21967efd6c00d105b31bdbd61f008a37b7b9fbf9
RenatoXD4 <renatochicahual@gmail.com>
2025-04-20 21:21:58 -0400

    fix: "Se arregla al obtener el json vacío en controlador profesionales"

--- servidor/controllers/profesionalesController.js
+++ servidor/controllers/profesionalesController.js
@@ -2,8 +2,8 @@ import { getProfesionales } from "../services/profesionalesService.js"
 
 export async function profesionales(req, res) {
     try{
-       const getProfs =  getProfesionales()
-       res.status(200).json(getProfs)
+       const getProfs =  await getProfesionales()
+       res.json(getProfs)
     }catch(e){
         console.log('Error al obtener los profesionales', e.message)
     }
