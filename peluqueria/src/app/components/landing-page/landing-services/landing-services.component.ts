ff37bb52b6269177661f2d3e999e6532a59f49dd
RenatoXD4 <renatochicahual@gmail.com>
2025-05-03 18:33:28 -0400

    chore: "Cambiando código en componentes de modal y landing services"

--- peluqueria/src/app/components/landing-page/landing-services/landing-services.component.ts
+++ peluqueria/src/app/components/landing-page/landing-services/landing-services.component.ts
@@ -1,10 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { ServiceCardComponent } from './service-card/service-card.component';
-import { ServiciosLandingService } from '@servicios/landingServices/servicio-service/servicios-landing.service';
-import { Servicio } from '@interfaces/servicio.interface';
 import { ModalReservaHoraComponent } from '@componentes/modal-reserva-hora/modal-reserva-hora.component';
-import { ProfesionalesService } from '@servicios/landingServices/profesionales-services/profesionales.service';
-import { Profesional } from '@interfaces/profesionales.interface';
 
 
 
@@ -15,8 +11,6 @@ import { Profesional } from '@interfaces/profesionales.interface';
   styleUrl: './landing-services.component.css'
 })
 export class LandingServicesComponent {
-  serviciosService = inject(ServiciosLandingService);
-  servicios =  signal<Servicio[]>(this.serviciosService.obtenerServicios());
   
   /* Variables Modal */
   isModalVisible= signal<boolean>(false);
--- peluqueria/src/app/components/modal-reserva-hora/modal-reserva-hora.component.ts
+++ peluqueria/src/app/components/modal-reserva-hora/modal-reserva-hora.component.ts
@@ -25,7 +25,6 @@ export class ModalReservaHoraComponent {
 
   /* Variables para el modal */
   isVisible = input<boolean>();
-  service = input.required<any>();
   close = output<void>();
   pasoActual: number = 1;
 
