a88a375b5dba51ba2445809f472226d1ea7896e6
RenatoXD4 <renatochicahual@gmail.com>
2025-05-05 14:28:33 -0400

    feat: "Añadiendo animaciones"

--- peluqueria/src/app/page/landing-page/landing-page.component.ts
+++ peluqueria/src/app/page/landing-page/landing-page.component.ts
@@ -4,6 +4,7 @@ import { LandingAboutUsComponent } from '../../components/landing-page/landing-a
 import { LandingServicesComponent } from '../../components/landing-page/landing-services/landing-services.component';
 import { LandingWelcomeComponent } from '../../components/landing-page/landing-welcome/landing-welcome.component';
 import { LandingMapComponent } from '../../components/landing-page/landing-map/landing-map.component';
+import { trigger, transition, style, animate, keyframes } from '@angular/animations';
 
 
 
@@ -13,7 +14,19 @@ import { LandingMapComponent } from '../../components/landing-page/landing-map/l
   standalone: true,
   imports: [LandingAboutUsComponent,LandingServicesComponent,LandingWelcomeComponent,LandingMapComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrl: './landing-page.component.css',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(-100%)' }),
+        animate('1.25s', keyframes([
+          style({ opacity: 0, transform: 'translateX(-300%)', offset: 0 }),
+          style({ opacity: 1, transform: 'translateX(15px)', offset: 0.7 }),
+          style({ opacity: 1, transform: 'translateX(0)', offset: 1 })
+        ]))
+      ])
+    ])
+  ]
 })
 export default class LandingPageComponent {
 
