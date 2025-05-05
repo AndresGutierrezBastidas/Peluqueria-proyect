8305462a3bbfe1bbfdec9647542db050faef24a5
RenatoXD4 <renatochicahual@gmail.com>
2025-05-05 14:44:58 -0400

    feat: "Usando carga diferida con animaciones"

--- peluqueria/src/app/page/landing-page/landing-page.component.ts
+++ peluqueria/src/app/page/landing-page/landing-page.component.ts
@@ -1,10 +1,10 @@
 
-import { Component, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { LandingAboutUsComponent } from '../../components/landing-page/landing-about-us/landing-about-us.component';
 import { LandingServicesComponent } from '../../components/landing-page/landing-services/landing-services.component';
 import { LandingWelcomeComponent } from '../../components/landing-page/landing-welcome/landing-welcome.component';
 import { LandingMapComponent } from '../../components/landing-page/landing-map/landing-map.component';
-import { trigger, transition, style, animate, keyframes } from '@angular/animations';
+import { trigger, transition, style, animate, keyframes, state } from '@angular/animations';
 
 
 
@@ -17,11 +17,11 @@ import { trigger, transition, style, animate, keyframes } from '@angular/animati
   styleUrl: './landing-page.component.css',
   animations: [
     trigger('fadeIn', [
+      state('void', style({ opacity: 0, transform: 'translateX(20px)' })),
       transition(':enter', [
-        style({ opacity: 0, transform: 'translateX(-100%)' }),
-        animate('1.25s', keyframes([
-          style({ opacity: 0, transform: 'translateX(-300%)', offset: 0 }),
-          style({ opacity: 1, transform: 'translateX(15px)', offset: 0.7 }),
+        animate('1.5s ease', keyframes([
+          style({ opacity: 0, transform: 'translateX(-100%)', offset: 0 }),
+          style({ opacity: 0.5, transform: 'translateX(-50%)', offset: 0.3 }),
           style({ opacity: 1, transform: 'translateX(0)', offset: 1 })
         ]))
       ])
