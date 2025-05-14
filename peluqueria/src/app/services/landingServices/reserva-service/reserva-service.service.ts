1b75bed5bdcdcbc5da11b07dc5b625a4e354ec81
Andrés Bastidas <65102015+AndresGutierrezBastidas@users.noreply.github.com>
2025-05-13 22:22:24 -0400

    fix:"arreglo de los servicios"

--- peluqueria/src/app/services/landingServices/reserva-service/reserva-service.service.ts
+++ peluqueria/src/app/services/landingServices/reserva-service/reserva-service.service.ts
@@ -6,25 +6,31 @@ import { Reserva } from '@interfaces/reserva.interface';
 @Injectable({
   providedIn: 'root',
 })
+
+//Manu y Miguel coordinar para el manejo de los servicio
+
+//[✅] obtener reserva
+//[✅] crear reserva
 export class ReservaServiceService {
   constructor() {}
 
   http = inject(HttpClient);
   url = 'http://localhost:3000/api/reserva';
   reservas = signal<Reserva[]>([]);
-
+//servicios para lo relacionado con las reservas
+//obtener reserva -> retorna todas las reservas
+//miguel encargado ser manejar el servicio de reserva
   obtenerReservas(): Reserva[] {
     this.http
       .get<Reserva[]>(`${this.url}/getReserva`)
       .subscribe((resp: Reserva[]) => {
         const reservas = adapter(resp)
         this.reservas.update((list) => [...list, ...reservas]);
-
       });
       console.log(this.reservas());
       return this.reservas();
   }
-
+//crear reserva asignado manu 
   crearReserva(reserva: any){
       this.http.post(`${this.url}/postReserva`,reserva)
       .subscribe({
--- peluqueria/src/app/services/landingServices/servicio-service/servicios-landing.service.ts
+++ peluqueria/src/app/services/landingServices/servicio-service/servicios-landing.service.ts
@@ -2,36 +2,65 @@ import { Servicio } from '@interfaces/servicio.interface';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { adapter } from '@adapter/commonAdapter';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ServiciosLandingService {
-  
+  //Miguel, Sebastian y manu (solo el obtener)
+  //[❗] deben revisar para que los servicios conicidan con el sistema de obtener
+  //[✅] obtener Servicios
+  //[✅] obtener servicios por id
+  //[✅] Crear servicios
+  //[❌] editar Editar servicios
+  constructor(){
+    this.obtenerServicios();
+  }
+
   http = inject(HttpClient);
   url = 'http://localhost:3000/api/services';
 
-  servicios = signal<Servicio[]>([]);
 
-  obtenerServicios(): Observable<Servicio[]>{
-    return this.http.get<Servicio[]>(`${this.url}/getServices`).pipe(
-      tap((resp : Servicio[]) => {
-      const servicios = adapter(resp)
-      this.servicios.update((list) => [...list,...servicios]);      
-    }));
+
+  servicios = signal<Map<number,Servicio>>(new Map());
+  serviciosArray = signal<Servicio[]>([]);
+
+
+  obtenerServicios():void{
+    this.http.get<Servicio[]>(`${this.url}/getServices`)
+    .pipe(
+      map(resp => adapter(resp))
+    ).
+    subscribe((resp:Servicio[]) => {
+      this.serviciosArray.set(resp);
+      const nuevoMap = new Map<number, Servicio>();
+      resp.forEach(serv => {
+        if(serv.id){
+          nuevoMap.set(serv.id,serv);
+        }
+      })
+      this.servicios.set(nuevoMap);
+    });
   }
 
 
-  crearServicio(servicio:Servicio){
-    this.http.post<Servicio>('http://localhost:3000/api/services/createServices',servicio).pipe(
-      tap( (item:Servicio) => this.servicios.update((list) => [...list,item])),
-    ).subscribe(
-      resp => {
-        return console.log(resp);
-      }
-    )
+  crearServicio(servicio: Servicio) {
+    this.http.post<{id: number} & Servicio>('http://localhost:3000/api/services/createServices', servicio).pipe(
+      tap((nuevoServicio) => {
+        this.servicios.update((currentMap) =>
+          currentMap.set(nuevoServicio.id, nuevoServicio)
+        );
+        this.serviciosArray.update(list => [...list, nuevoServicio]);
+      })
+    ).subscribe(resp => {
+      console.log("servicio creado: "+ resp);
+    });
+  }
+
+  obtenerServicioNombreId(id:number){
+    return this.servicios().get(id)?.nombre;
   }
 
 
