75d0648fc85b0fcaa36c80e7f128ab6ed0be8fe2
Manuel <manuelenrriquerivera@gmail.com>
2025-05-15 16:34:32 -0400

    feature: "Profesionales por servicio"

--- servidor/services/profesionalesService.js
+++ servidor/services/profesionalesService.js
@@ -10,5 +10,29 @@ export async function getProfesionales() {
     }
 }
 
+export async function getProfServicio(servicioId){
+    try {
+        /* RawSql */
+        /* const profServicio = await prisma.$queryRaw(prisma.sql`
+        SELECT p.* 
+        FROM profesional p 
+        JOIN servicioprofesional sp ON (sp.profesionalId = p.id)
+        WHERE sp.servicioId = ${id}
+        GROUP BY p.id`); */
 
-
+        /* PrismaSql */
+        const profServicio = await prisma.profesional.findMany({
+            where: {
+                serPro:{
+                    some:{
+                        servicioId: servicioId,
+                    }
+                }
+            },
+        });
+        return profServicio;
+    } catch (error) {
+        console.error("Error en getProfServicio", error.message);
+        throw error;
+    }
+}
